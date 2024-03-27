79f80ee7d7e25db13bbc35aed202dba42762c792
Sada <shukurova.sada@gmail.com>
2024-03-27 18:12:33 +0400

    http requests

---
+++ users.js
@@ -0,0 +1,23 @@
+let users = [
+  {
+    id: 0,
+    name: "Sada",
+    age: 30,
+  },
+  {
+    id: 1,
+    name: "Sabina",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Ruslan",
+    age: 23,
+  },
+  {
+    id: 3,
+    name: "Canaga",
+    age: 20,
+  },
+];
+export default users;
